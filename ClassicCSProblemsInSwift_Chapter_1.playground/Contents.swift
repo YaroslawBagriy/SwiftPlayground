5c6723aeba50e2f580aa0c015888ffa7cb6d35b2
Yaroslaw Bagriy <yaroslawbagriy@gmail.com>
2018-10-27 16:05:37 -0500

    Added code for section 1.2 from Chapter 1 from the Classic Computer Science Problems in Swift

--- ClassicCSProblemsInSwift_Chapter_1.playground/Contents.swift
+++ ClassicCSProblemsInSwift_Chapter_1.playground/Contents.swift
@@ -54,3 +54,69 @@ func fib4(n: UInt) -> UInt {
 }
 
 // Naive recursive solutions can come with significant performance costs.
+
+
+// 1.2 Trivial Compression
+
+struct CompressedGene {
+    let length: Int
+    private let bitVector: CFMutableBitVector
+    
+    init(original: String) {
+        length = original.count
+        // Default allocator, need 2 * length number of bits
+        bitVector = CFBitVectorCreateMutable(kCFAllocatorDefault, length * 2)
+        // Fills the bit vector with 0s
+        CFBitVectorSetCount(bitVector, length * 2)
+        compress(gene: original)
+    }
+    
+    func decompress() -> String {
+        var gene: String = ""
+        for index in 0..<length {
+            // Start of each nucleotide
+            let nStart = index * 2
+            let firstBit = CFBitVectorGetBitAtIndex(bitVector, nStart)
+            let secondBit = CFBitVectorGetBitAtIndex(bitVector, nStart + 1)
+            switch(firstBit, secondBit) {
+                case (0, 0): // 00 A
+                    gene += "A"
+                case (0, 1): // 01 C
+                    gene += "C"
+                case (1, 0): // 01 G
+                    gene += "G"
+                case (1, 1): // 11 T
+                    gene += "T"
+                default:
+                    break // Unreachable, but need default
+            }
+        }
+        
+        return gene
+    }
+    
+    private func compress(gene: String) {
+        for (index, nucleotide) in gene.uppercased().enumerated() {
+            // Start of each new nucleotide
+            let nStart = index * 2
+            switch nucleotide {
+                case "A": // 00
+                    CFBitVectorSetBitAtIndex(bitVector, nStart, 0)
+                    CFBitVectorSetBitAtIndex(bitVector, nStart + 1, 0)
+                case "C": // 01
+                    CFBitVectorSetBitAtIndex(bitVector, nStart, 0)
+                    CFBitVectorSetBitAtIndex(bitVector, nStart + 1, 1)
+                case "G": // 10
+                    CFBitVectorSetBitAtIndex(bitVector, nStart, 1)
+                    CFBitVectorSetBitAtIndex(bitVector, nStart + 1, 0)
+                case "T": // 11
+                    CFBitVectorSetBitAtIndex(bitVector, nStart, 1)
+                    CFBitVectorSetBitAtIndex(bitVector, nStart + 1, 1)
+                default:
+                    print("Unexpected character \(nucleotide) at \(index)")
+            }
+        }
+    }
+}
+
+print(CompressedGene(original: "ACCTGGCATTGCA").decompress())
